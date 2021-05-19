dfe66cd6887c7a18ed9159c223f97b42f63fde6f
Greatdane <david@setoka.co>
2021-05-19 14:40:10 +0900

    Added small packet, DB cleanup

--- JapanPostAPI/api/admin.py
+++ JapanPostAPI/api/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Country
+from .models import Country, International_Parcel_Post, EMS, Small_Packet
 
 admin.site.register(Country)
+admin.site.register(International_Parcel_Post)
+admin.site.register(EMS)
+admin.site.register(Small_Packet)
--- JapanPostAPI/api/models.py
+++ JapanPostAPI/api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Country(models.Model):
     country_en = models.CharField(max_length=200)
     country_jp = models.CharField(max_length=200)
@@ -9,6 +10,7 @@ class Country(models.Model):
 
     def __str__(self):
         return self.country_en
+
 
 class International_Parcel_Post(models.Model):
     weight = models.IntegerField()
@@ -20,6 +22,14 @@ class International_Parcel_Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        verbose_name = 'International Parcel Post'
+        verbose_name_plural = 'International Parcel Post'
+
+    def __str__(self):
+        return 'Zone: %s - Weight: %s' % (self.zone, self.weight)
+
+
 class EMS(models.Model):
     weight = models.IntegerField()
     zone = models.CharField(max_length=3)
@@ -27,3 +37,28 @@ class EMS(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'EMS'
+        verbose_name_plural = 'EMS'
+
+    def __str__(self):
+        return '%s - %s' % (self.zone, self.weight)
+
+
+class Small_Packet(models.Model):
+    weight = models.IntegerField()
+    zone = models.IntegerField()
+    airmail_price = models.IntegerField(blank=True, null=True)
+    SAL_price = models.IntegerField(blank=True, null=True)
+    surface_price = models.IntegerField(blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Small Packet'
+        verbose_name_plural = 'Small Packet'
+
+    def __str__(self):
+        return 'Zone: %s - Weight: %s' % (self.zone, self.weight)
