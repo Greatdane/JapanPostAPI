fb2483cb2f4dd2d5ed8db7ec47b81fb4eb140b47
Greatdane <david@setoka.co>
2021-05-20 13:04:40 +0900

    added basic country search

--- JapanPostAPI/api/urls.py
+++ JapanPostAPI/api/urls.py
@@ -1,6 +1,3 @@
-from rest_framework import routers
-from .api import International_Parcel_Post_ViewSet, Country_Viewset
-
 from django.urls import path, include
 from .views import CountryAPIView
 
--- JapanPostAPI/api/views.py
+++ JapanPostAPI/api/views.py
@@ -1,10 +1,12 @@
 # from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, filters
 
 from .models import Country
 from .serializers import Country_Serializer
 
 
 class CountryAPIView(generics.ListCreateAPIView):
+    search_fields = ['country_en']
+    filter_backends = [filters.SearchFilter]
     queryset = Country.objects.all()
     serializer_class = Country_Serializer
